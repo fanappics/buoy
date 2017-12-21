5b66cc1b9803e112dff31b27d89e069c3aa49bbf
malear@fanatics.com <malear@fanatics.com>
2017-12-21 06:33:54 -0700

    compiles fine without v-app, on to circle

--- dev/Playground.vue
+++ dev/Playground.vue
@@ -1,11 +1,27 @@
 <template>
-  <v-app>
-  </v-app>
+  <div>
+    <BSelect
+            v-bind:selectOptions=options
+            v-bind:isRequired=false
+            selectLabel="two"
+        />
+  </div>
 </template>
 
 <script>
+  import BSelect from '../src/components/BSelect/BSelect.vue'
+
+  const options = [{id: 1, text: 'a'}, {id: 2, text: 'b'}, {id: 3, text: 'c'}]
+
   export default {
-    data: () => ({
-    })
+    components: {
+      BSelect
+    },
+  data () {
+    return {
+      options: options,
+
+    }
   }
+}
 </script>
