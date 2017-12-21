56a85011ee8b1900c5ed0a70de6f5c0b1479314d
malear@fanatics.com <malear@fanatics.com>
2017-12-20 17:47:32 -0700

    done with select

--- src/components/BSelect/BSelect.test.js
+++ src/components/BSelect/BSelect.test.js
@@ -4,8 +4,10 @@ import BSelect from './BSelect'
 describe('BSelect', () => {
   it('creates options when passed', () => {
     const selectOptions = [{id: 1, text: 'test'}]
+    const isRequired = false
+    const selectLabel = 'test'
     const wrapper = shallow(BSelect, {
-      propsData: { selectOptions }
+      propsData: { selectOptions, isRequired, selectLabel }
     })
     expect(wrapper.props().selectOptions.length).toBe(selectOptions.length)
   })
