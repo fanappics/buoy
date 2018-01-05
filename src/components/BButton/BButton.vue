18212c1a87a850dbc218adbc8c0d783b13719829
David Sudia <dsudia@fanatics.com>
2018-01-05 13:40:52 -0700

    initial fulfillment of button requirements

--- src/components/BButton/BButton.vue
+++ src/components/BButton/BButton.vue
@@ -1,18 +1,75 @@
 <template>
-  <button>{{text}}</button>
+  <button
+    v-bind:class="computedClass"
+    v-bind:disabled="disabled"
+    v-bind:type="type"
+    v-bind:height="height"
+    v-bind:width="width"
+    v-bind:style="computedStyle"
+    v-bind:aria-label="'aria-label'"
+  >
+    <slot></slot>
+  </button>
 </template>
 
 <script>
 export default {
   name: 'b-button',
-  props: [
-    'text'
-  ],
+  props: {
+    'aria-label': {
+      type: String,
+      required: false
+    },
+    class: {
+      type: String,
+      required: false
+    },
+    disabled: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+    height: {
+      type: String,
+      required: false
+    },
+    type: {
+      type: String,
+      required: false,
+      default: 'button'
+    },
+    width: {
+      type: String,
+      required: false
+    }
+  },
   data () {
     return {}
+  },
+  computed: {
+    computedClass () {
+      // if any classes are passed through, provided them to the component as an array of strings
+      const classArray = this.class ? this.class.split(' ') : [];
+
+      // can do differential class application in the future here, i.e.,
+      // we may want to break out 'color' as a prop to improve readability:
+      // if (this.color && !classArray.includes(this.color)) {
+        // classArray.push(this.color)
+      // }
+
+      return classArray
+    },
+    computedStyle () {
+      // allow size to be adjusted via props
+      const styleObject = {}
+      if (this.height) {
+        styleObject.height = this.height
+      }
+      if (this.width) {
+        styleObject.width = this.width
+      }
+      return styleObject
+    }
   }
 }
 </script>
-
-<style scoped>
-</style>
