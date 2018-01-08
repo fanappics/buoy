bddd36c87dd33c0eff683db2a20a667bc81b20af
swade <swade@C02R58FEG8WP.local>
2018-01-08 10:36:48 -0700

    name issues

--- src/components/BTextarea/index.js
+++
@@ -1,9 +0,0 @@
-// import BTextInput from './BTextInput.vue'
-import BTextarea from './BTextarea.vue'
-
-/* istanbul ignore next */
-BTextarea.install = function install (Vue) {
-  Vue.component(BTextarea.name, BTextarea)
-}
-
-export default BTextarea
