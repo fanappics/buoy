d21532a6f141b9341a58ed30e9fda82aca8665af
Mathew Brown <matbrown@fanatics.com>
2018-01-08 09:20:35 -0700

    Updating test

--- src/components/BContainer/BContainer.test.js
+++ src/components/BContainer/BContainer.test.js
@@ -9,15 +9,23 @@ describe('BContainer', () => {
     })
     expect(wrapper.html()).toMatchSnapshot()
   })
-  it(`renders props.label in H1 when passed`, () => {
+  it(`renders props.label in header element when passed.  Header element defaults to H1`, () => {
     const label = 'hello world'
     const wrapper = mount(BContainer, {
       propsData: { label }
     })
     expect(wrapper.find('h1').text()).toBe(label)
   })
-  it(`shouldn't render an H1 when no props.label is passed.`, () => {
+  it(`shouldn't render a H1 element when no props.label is passed.`, () => {
     const wrapper = mount(BContainer)
     expect(wrapper.findAll('h1').length).toBe(0)
+  })
+  it(`renders props.label in header element when passed.  Can specify header level with prop`, () => {
+    const label = 'hello world'
+    const level = 6
+    const wrapper = mount(BContainer, {
+      propsData: { label, level }
+    })
+    expect(wrapper.find('h6').text()).toBe(label)
   })
 })
