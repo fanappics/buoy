f66e3de8db30938bd46b472c0da158e411498856
David Sudia <dsudia@gmail.com>
2018-01-09 09:41:18 -0700

    BButton element (#12)
    * initial fulfillment of button requirements
    
    * change to shorthand for v-bind
    
    * add tests
    
    * waiting on response on gitter for class issue
    
    * change it to take snapshot showing disabled prop is not applied
    
    * fixed so that ariaLabel works correctly
    
    * refactor how class is handled
    
    * removed disabled prop pass test
    
    * add new test snapshot for aria-label test, update comments
    
    * reordered element attributes

--- src/components/BButton/BButton.vue
+++ src/components/BButton/BButton.vue
@@ -1,18 +1,72 @@
 <template>
-  <button>{{text}}</button>
+  <button
+    :aria-label="ariaLabel"
+    :class="computedClass"
+    :disabled="disabled"
+    :style="computedStyle"
+  >
+    <slot></slot>
+  </button>
 </template>
 
 <script>
 export default {
   name: 'b-button',
-  props: [
-    'text'
-  ],
+  props: {
+    'ariaLabel': {
+      type: String,
+      required: false
+    },
+    color: {
+      type: String,
+      required: false
+    },
+    disabled: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+    height: {
+      type: String,
+      required: false
+    },
+    type: {
+      type: String,
+      required: false,
+      default: 'button'
+    },
+    width: {
+      type: String,
+      required: false
+    }
+  },
   data () {
     return {}
+  },
+  computed: {
+    computedClass () {
+      const classObj = {}
+
+      // get current classes and check if abstracted classes are already present
+      // don't copy them if so, otherwise, put them in classObj
+      const classArray = this.class ? this.class.split(' ') : [];
+      if (this.color && !classArray.includes(this.color)) {
+        classObj[this.color] = this.color
+      }
+
+      return classObj
+    },
+    computedStyle () {
+      // allow size to be adjusted via props
+      const styleObject = {}
+      if (this.height) {
+        styleObject.height = this.height
+      }
+      if (this.width) {
+        styleObject.width = this.width
+      }
+      return styleObject
+    }
   }
 }
 </script>
-
-<style scoped>
-</style>
