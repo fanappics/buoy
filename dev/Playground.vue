25cf372404210ec3575ba29eafcbb58ab28b15ce
Mathew Brown <matt@mattbrown.com>
2018-01-11 11:41:11 -0700

    BContainer element (CPDT-89) (#14)
    * Create BContainer component
    
    * Update live style guide
    
    * Update live style guide
    
    * Flexbox-ify
    
    * Add level functionality
    
    * Build docs
    
    * Updating test
    
    * Convert pixels to rem
    
    * Update Validator
    
    * Playground
    
    * Make level required if a label is passed as a prop.
    
    * Add aria-label and contenttype to section.
    Add hideLabel boolean prop.
    Require a label to be used for aria-label.
    Output warnings and errors for invalid headerLevel prop.

--- dev/Playground.vue
+++ dev/Playground.vue
@@ -1,6 +1,26 @@
 <template>
-  <v-app>
-  </v-app>
+  <div>
+    <b-container label="test-default-h2">
+        <button>Test</button>
+        <button>Test2</button> 
+    </b-container>
+    <b-container label="test-h3" headerLevel="3">
+        <button>Test</button>
+        <button>Test2</button> 
+    </b-container>
+    <b-container label="test-h3-hidelabel" headerLevel="3" hideLabel>
+        <button>Test</button>
+        <button>Test2</button> 
+    </b-container>
+    <b-container label="test-h3-hidelabel" headerLevel="6">
+        <button>Test</button>
+        <button>Test2</button> 
+    </b-container>
+    <b-container label="test-error" headerLevel="the greatest header">
+        <button>Test</button>
+        <button>Test2</button> 
+    </b-container>
+  </div>
 </template>
 
 <script>
