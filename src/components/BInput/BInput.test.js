6f14b79fc85e813b19adab2662ff596ef7cc9986
fobesjason <fobes.jason@gmail.com>
2018-01-15 09:47:47 -0700

    Ft cpdt 80 (#17)
    This is the text input component for buoy.
    
    It should be able to handle these text-based input types:
    text (default), date, datetime-local, email, month, number, password, search, tel, time, url, week, and currency (a custom type for dollar amount inputs)
    
    Required props when using b-input include:
    id: id put on input element
    label: text put into the label for the input
    
    Optional props you can pass:
    autofocus, disabled, max, maxlength, min, minlength, name, pattern, placeholder, readonly, required, type, and value
    
    BInput supports v-model to retrieve the input data. A default value of the input can be set through the field binded to v-model.
    
    VeeValidate support for the following:
    Required, min, minvalue, max, maxvalue, email, and url
    Input type tel has a validation to require 10 digits
    Input type currency has a validation to allow up to 2 decimal places

---
+++ src/components/BInput/BInput.test.js
@@ -0,0 +1,153 @@
+import { createLocalVue, shallow } from 'vue-test-utils'
+import VeeValidate from 'vee-validate'
+import BInput from './BInput'
+
+const localVue = createLocalVue()
+localVue.use(VeeValidate)
+
+describe('Text input', () => {
+  it('mounted html should match snapshot', () => {
+    const wrapper = shallow(BInput, {
+      propsData: {
+        id: 'input-1',
+        label: 'My Input'
+      },
+      localVue
+    })
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+})
+
+describe('Number input', () => {
+  it('mounted html should match snapshot', () => {
+    const wrapper = shallow(BInput, {
+      propsData: {
+        id: 'input-1',
+        label: 'My Input',
+        type: 'number'
+      },
+      localVue
+    })
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+})
+
+describe('Currency input', () => {
+  it('mounted html should match snapshot', () => {
+    const wrapper = shallow(BInput, {
+      propsData: {
+        id: 'input-1',
+        label: 'My Input',
+        type: 'currency'
+      },
+      localVue
+    })
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+})
+
+describe('Disabled input', () => {
+  it('mounted html should match snapshot', () => {
+    const wrapper = shallow(BInput, {
+      propsData: {
+        id: 'input-1',
+        label: 'My Input',
+        disabled: true
+      },
+      localVue
+    })
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+})
+
+describe('Required input', () => {
+  it('mounted html should match snapshot', () => {
+    const wrapper = shallow(BInput, {
+      propsData: {
+        id: 'input-1',
+        label: 'My Input',
+        required: true
+      },
+      localVue
+    })
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+})
+
+describe('Pattern input', () => {
+  it('mounted html should match snapshot', () => {
+    const wrapper = shallow(BInput, {
+      propsData: {
+        id: 'input-1',
+        label: 'My Input',
+        pattern: '^([0-9]+)$'
+      },
+      localVue
+    })
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+})
+
+describe('Readonly input', () => {
+  it('mounted html should match snapshot', () => {
+    const wrapper = shallow(BInput, {
+      propsData: {
+        id: 'input-1',
+        label: 'My Input',
+        readonly: true
+      },
+      localVue
+    })
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+})
+
+describe('Named input', () => {
+  it('mounted html should match snapshot', () => {
+    const wrapper = shallow(BInput, {
+      propsData: {
+        id: 'input-1',
+        label: 'My Input',
+        name: 'Name of Input'
+      },
+      localVue
+    })
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+})
+
+describe('Autofocused input', () => {
+  it('mounted html should match snapshot', () => {
+    const wrapper = shallow(BInput, {
+      propsData: {
+        id: 'input-1',
+        label: 'My Input',
+        autofocus: true
+      },
+      localVue
+    })
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+})
+
+describe('Required input failing validation', () => {
+  it('mounted html should match snapshot', async () => {
+    const wrapper = shallow(BInput, {
+      propsData: {
+        id: 'input-1',
+        label: 'My Input',
+        required: true
+      },
+      localVue
+    })
+
+    const input = wrapper.find('input')
+    input.trigger('focus')
+    await wrapper.vm.$nextTick()
+    input.element.value = ' '
+    input.trigger('input')
+    await wrapper.vm.$nextTick()
+
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+})
