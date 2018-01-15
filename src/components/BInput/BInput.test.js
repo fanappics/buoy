1b40a0b6c0399aebd6876727225f246551ec79d2
Artem Vovk <avovk@fanatics.com>
2018-01-15 10:06:54 -0700

    lint fixer for other branches

--- src/components/BInput/BInput.test.js
+++ src/components/BInput/BInput.test.js
@@ -1,22 +1,22 @@
-import { createLocalVue, shallow } from 'vue-test-utils'
-import VeeValidate from 'vee-validate'
-import BInput from './BInput'
+import { createLocalVue, shallow } from 'vue-test-utils';
+import VeeValidate from 'vee-validate';
+import BInput from './BInput';
 
-const localVue = createLocalVue()
-localVue.use(VeeValidate)
+const localVue = createLocalVue();
+localVue.use(VeeValidate);
 
 describe('Text input', () => {
   it('mounted html should match snapshot', () => {
     const wrapper = shallow(BInput, {
       propsData: {
         id: 'input-1',
-        label: 'My Input'
+        label: 'My Input',
       },
-      localVue
-    })
-    expect(wrapper.html()).toMatchSnapshot()
-  })
-})
+      localVue,
+    });
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+});
 
 describe('Number input', () => {
   it('mounted html should match snapshot', () => {
@@ -24,13 +24,13 @@ describe('Number input', () => {
       propsData: {
         id: 'input-1',
         label: 'My Input',
-        type: 'number'
+        type: 'number',
       },
-      localVue
-    })
-    expect(wrapper.html()).toMatchSnapshot()
-  })
-})
+      localVue,
+    });
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+});
 
 describe('Currency input', () => {
   it('mounted html should match snapshot', () => {
@@ -38,13 +38,13 @@ describe('Currency input', () => {
       propsData: {
         id: 'input-1',
         label: 'My Input',
-        type: 'currency'
+        type: 'currency',
       },
-      localVue
-    })
-    expect(wrapper.html()).toMatchSnapshot()
-  })
-})
+      localVue,
+    });
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+});
 
 describe('Disabled input', () => {
   it('mounted html should match snapshot', () => {
@@ -52,13 +52,13 @@ describe('Disabled input', () => {
       propsData: {
         id: 'input-1',
         label: 'My Input',
-        disabled: true
+        disabled: true,
       },
-      localVue
-    })
-    expect(wrapper.html()).toMatchSnapshot()
-  })
-})
+      localVue,
+    });
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+});
 
 describe('Required input', () => {
   it('mounted html should match snapshot', () => {
@@ -66,13 +66,13 @@ describe('Required input', () => {
       propsData: {
         id: 'input-1',
         label: 'My Input',
-        required: true
+        required: true,
       },
-      localVue
-    })
-    expect(wrapper.html()).toMatchSnapshot()
-  })
-})
+      localVue,
+    });
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+});
 
 describe('Pattern input', () => {
   it('mounted html should match snapshot', () => {
@@ -80,13 +80,13 @@ describe('Pattern input', () => {
       propsData: {
         id: 'input-1',
         label: 'My Input',
-        pattern: '^([0-9]+)$'
+        pattern: '^([0-9]+)$',
       },
-      localVue
-    })
-    expect(wrapper.html()).toMatchSnapshot()
-  })
-})
+      localVue,
+    });
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+});
 
 describe('Readonly input', () => {
   it('mounted html should match snapshot', () => {
@@ -94,13 +94,13 @@ describe('Readonly input', () => {
       propsData: {
         id: 'input-1',
         label: 'My Input',
-        readonly: true
+        readonly: true,
       },
-      localVue
-    })
-    expect(wrapper.html()).toMatchSnapshot()
-  })
-})
+      localVue,
+    });
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+});
 
 describe('Named input', () => {
   it('mounted html should match snapshot', () => {
@@ -108,13 +108,13 @@ describe('Named input', () => {
       propsData: {
         id: 'input-1',
         label: 'My Input',
-        name: 'Name of Input'
+        name: 'Name of Input',
       },
-      localVue
-    })
-    expect(wrapper.html()).toMatchSnapshot()
-  })
-})
+      localVue,
+    });
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+});
 
 describe('Autofocused input', () => {
   it('mounted html should match snapshot', () => {
@@ -122,13 +122,13 @@ describe('Autofocused input', () => {
       propsData: {
         id: 'input-1',
         label: 'My Input',
-        autofocus: true
+        autofocus: true,
       },
-      localVue
-    })
-    expect(wrapper.html()).toMatchSnapshot()
-  })
-})
+      localVue,
+    });
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+});
 
 describe('Required input failing validation', () => {
   it('mounted html should match snapshot', async () => {
@@ -136,18 +136,18 @@ describe('Required input failing validation', () => {
       propsData: {
         id: 'input-1',
         label: 'My Input',
-        required: true
+        required: true,
       },
-      localVue
-    })
+      localVue,
+    });
 
-    const input = wrapper.find('input')
-    input.trigger('focus')
-    await wrapper.vm.$nextTick()
-    input.element.value = ' '
-    input.trigger('input')
-    await wrapper.vm.$nextTick()
+    const input = wrapper.find('input');
+    input.trigger('focus');
+    await wrapper.vm.$nextTick();
+    input.element.value = ' ';
+    input.trigger('input');
+    await wrapper.vm.$nextTick();
 
-    expect(wrapper.html()).toMatchSnapshot()
-  })
-})
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+});
