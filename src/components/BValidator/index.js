f665b199b6f2c22f0cacd1aca1dc25df69d1ee65
Jason <fobes.jason@gmail.com>
2018-01-15 11:01:58 -0700

    Added b-validator, no tests added

---
+++ src/components/BValidator/index.js
@@ -0,0 +1,8 @@
+import BValidator from './BValidator.vue'
+
+/* istanbul ignore next */
+BValidator.install = function install (Vue) {
+  Vue.component(BValidator.name, BValidator)
+}
+
+export default BValidator
