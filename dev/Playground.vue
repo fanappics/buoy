519ddcc90a3808951f0aca6d64b78af48b8760f3
Mathew Brown <matbrown@fanatics.com>
2018-01-16 12:42:46 -0700

    Playground

--- dev/Playground.vue
+++ dev/Playground.vue
@@ -1,57 +1,56 @@
 <template>
   <div>
-    <b-checkbox v-bind="checkbox" />
-    <br />
-    <b-checkbox v-bind="checkboxGroup1" />
-    <br />
-    <b-checkbox v-bind="checkboxGroup2" />
-    <br />
-    <b-checkbox v-bind="checkboxGroup3" />
-    <br />
-    <b-checkbox v-bind="checkboxGroup4" />
-    <br />
-    <b-checkbox v-bind="checkboxGroup5" />
+    <b-validator scope="root" v-model="rootValid">
+      <b-validator scope="scope" v-model="valid">
+        <b-checkbox v-bind="checkbox" required/>
+        <br />
+        <b-checkbox v-bind="checkboxGroup1" required/>
+        <br />
+        {{ valid }}
+      </b-validator>
+
+      <b-validator scope="scope2" v-model="valid2">
+        <b-checkbox v-bind="checkbox2" required/>
+        <br />
+        <b-checkbox v-bind="checkboxGroup2" required/>
+        <br />
+        {{ valid2 }}
+      </b-validator>
+
+      <button :disabled="!rootValid">Submit</button>
+    </b-validator>
   </div>
 </template>
 
 <script>
   import BCheckbox from '../src/components/BCheckbox'
+  import BValidator from '../src/components/BValidator'
+
   export default {
     components: {
-      BCheckbox
+      BCheckbox,
+      BValidator
     },
     data: () => ({
       checkbox: {
         checkboxes: [{id: 'checkbox-1', value: 'Sign up now'}]
       },
       checkboxGroup1: {
-        checkboxes: [{id: 'checkbox-2', value: 'Forward'}, {id: 'checkbox-3', value: 'Backward'}]
+        checkboxes: [{id: 'checkbox-2', value: 'Forward'}, {id: 'checkbox-3', value: 'Backward'}],
+        groupId: 'checkbox-group',
+        groupLabel: 'Direction:'
+      },
+      checkbox2: {
+        checkboxes: [{id: 'checkbox-4', value: 'Sign up now'}]
       },
       checkboxGroup2: {
-        checkboxes: [{id: 'checkbox-4', value: 'Forward'}, {id: 'checkbox-5', value: 'Backward'}],
+        checkboxes: [{id: 'checkbox-5', value: 'Forward'}, {id: 'checkbox-6', value: 'Backward'}],
         groupId: 'checkbox-group',
-        groupLabel: 'Direction:',
-        showGroupLabel: true
+        groupLabel: 'Direction:'
       },
-      checkboxGroup3: {
-        checkboxes: [{id: 'checkbox-6', value: 'Forward', checked: true}, {id: 'checkbox-7', value: 'Backward'}],
-        groupId: 'checkbox-group',
-        groupLabel: 'Direction:',
-        showGroupLabel: true
-      },
-      checkboxGroup4: {
-        checkboxes: [{id: 'checkbox-8', value: 'Forward'}, {id: 'checkbox-9', value: 'Backward', disabled: true}],
-        groupId: 'checkbox-group',
-        groupLabel: 'Direction:',
-        showGroupLabel: true
-      },
-      checkboxGroup5: {
-        checkboxes: [{id: 'checkbox-10', value: 'Forward'}, {id: 'checkbox-11', value: 'Backward'}],
-        groupId: 'checkbox-group',
-        groupLabel: 'Direction:',
-        showGroupLabel: true,
-        disabled: true
-      }
+      valid: false,
+      valid2: false,
+      rootValid: false
     })
   }
 </script>
