74c51ba31354fdf415023da97989f8fb1e3d55d6
malear@fanatics.com <malear@fanatics.com>
2018-01-16 13:52:37 -0700

    more documentation for bselect

--- src/components/BSelect/BSelect.vue
+++ src/components/BSelect/BSelect.vue
@@ -61,25 +61,43 @@ export default {
   name: 'b-select',
   validator: null,
   props: {
+    /**
+    * Component Id
+    */
     id: {
       type: String,
       required: true
     },
+    /**
+    * Array of objects to be displayed as options.
+    */
     selectOptions: {
       type: Array
     },
+    /**
+    * Use to turn on validation and to indicate field is required.
+    */
     required: {
       type: Boolean,
       required: true
     },
+    /**
+    * Label of component.
+    */
     selectLabel: {
       type: String,
       required: true
     },
+    /**
+    * If included, this object will be pre-selected.
+    */
     initialValue: {
       type: Object,
       required: false
     },
+    /**
+    * If no preselection, this is the placeholder text for the component.
+    */
     placeholder: {
       type: String,
       required: false,
@@ -293,3 +311,41 @@ export default {
  }
 
 </style>
+
+<docs>
+```js
+
+<div>
+  <div style="width: 50%">
+    <b-select
+      :required=false
+      :selectOptions="[{id: 1, text: 'This'}, {id: 2, text: 'Is'}, {id: 3, text: 'An Example'}]"
+      id="example-id"
+      placeholder="Placeholder Example"
+      selectLabel="Select Example"
+    />
+  </div>
+
+  <div style="width: 50%">
+    <b-select
+      :required=true
+      :selectOptions="[{id: 1, text: 'This'}, {id: 2, text: 'Is'}, {id: 3, text: 'An Example'}]"
+      id="example-id-2"
+      placeholder="Placeholder Example"
+      selectLabel="Required Select Example"
+    />
+  </div>
+
+  <div style="width: 50%">
+    <b-select
+      :initialValue="{id: 3, text: 'An Example'}"
+      :required=true
+      :selectOptions="[{id: 1, text: 'This'}, {id: 2, text: 'Is'}, {id: 3, text: 'An Example'}]"
+      id="example-id-3"
+      selectLabel="Pre Selected Example"
+    />
+  </div>
+</div>
+```
+
+</docs>
