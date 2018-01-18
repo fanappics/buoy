ad1e43660fced9edc7d6e9c2bdbe1901531b9659
Mathew Brown <matbrown@fanatics.com>
2018-01-18 09:49:52 -0700

    Fix conflicts

--- dev/Playground.vue
+++ dev/Playground.vue
@@ -1,22 +1,5 @@
 <template>
   <div>
-<<<<<<< HEAD
-  <b-validator scope="parent" v-model="valid">
-    <b-validator scope="child1">
-      <b-input id="email" type="email" label="email" required/>
-    </b-validator>
-    <b-validator scope="child2">
-      <b-input id="password" type="password" label="Password" v-model="password" required />
-      <b-input id="password2" type="password" :pattern="`^${password}`" label="Retype password" required/>
-    </b-validator>
-
-    {{ valid }}
-
-    <b-button :disabled="!valid">Log In</b-button>
-
-  </b-validator>
-=======
->>>>>>> a3a9726bd2d387528be9bce35253c2febca81836
   </div>
 </template>
         
@@ -27,12 +10,6 @@
     },
     data () {
       return {
-<<<<<<< HEAD
-        toggleValue: false,
-        password: '',
-        valid: false
-=======
->>>>>>> a3a9726bd2d387528be9bce35253c2febca81836
       }
     }
   }
