9a384c4b087fc6bf2b697f49eb19f75311e44365
David Sudia <dsudia@gmail.com>
2018-01-18 12:07:40 -0700

    change to expandable container

--- src/components/BContainer/BContainer.vue
+++ src/components/BContainer/BContainer.vue
@@ -1,9 +1,20 @@
 <template>
   <section role="contentinfo" :aria-label="label">
-    <component v-if=!hideLabel :is=headerElement() class="header">{{ label }}</component>
     <div>
-      <slot></slot>
+      <span
+        :class="{ 'caret': true, 'caret-down': expanded}"
+        v-if="expandable"
+        @click="toggleCaret"
+      >
+      </span>
+      <component v-if=!hideLabel :is=headerElement() class="header">{{ label }}</component>
+      <slot name="icon" class="icon"></slot>
     </div>
+    <transition name="fade">
+      <div class="bordered" v-if="expanded">
+        <slot></slot>
+      </div>
+    </transition>
   </section>
 </template>
 
@@ -32,6 +43,9 @@ export default {
     */
     hideLabel: {
       type: Boolean
+    },
+    expandable: {
+      type: Boolean
     }
   },
   created: function() {
@@ -53,23 +67,56 @@ export default {
         } else {
           return `h${this.headerLevel}`
         }
-      }
+      },
+      expanded: true
+    }
+  },
+
+  methods: {
+    toggleCaret () {
+      this.caretToggle = !this.caretToggle
+      this.expanded = !this.expanded
     }
   }
 }
 </script>
 
 <style scoped>
-  .header {
-    margin: 0.125rem 0.3125rem;
-  }
-  div {
-    display: flex;
+  .bordered {
     flex-grow: 1;
     border:1px solid rgb(217,217,217);
     border-radius: 0.1875rem;
     margin: 0.125rem 0.3125rem;
     padding: 0.625rem;
+  }
+  .caret {
+    width: 0; 
+    height: 0; 
+    border-top: .4rem solid transparent;
+    border-bottom: .4rem solid transparent;
+    border-left: .4rem solid blue;
+    margin-left: .4rem;
+    margin-right: .5rem;
+    margin-bottom: .2rem;
+    transition: all .3s;
+  }
+  .caret-down {
+    transition: all .3s;
+    transform: rotate(90deg);
+  }
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity .3s;
+  }
+  .fade-enter, .fade-leave-to {
+    opacity: 0;
+  }
+  .header {
+    margin: 0.125rem 0.3125rem;
+    flex-grow: 1
+  }
+  div {
+    display: flex;
+    align-items: center
   }
   section {
     display: flex;
@@ -79,7 +126,8 @@ export default {
 </style>
 <docs>
 ```js
-<b-container class="edit_me" label="Container Header" headerLevel="3">
+<b-container class="edit_me" label="Container Header" headerLevel="3" expandable>
+  <span slot="icon">&#10004;</span>
   <b-button>Test</b-button>
   <b-button>Test2</b-button> 
 </b-container>
--- src/components/BDetails/index.js
+++
@@ -1,8 +0,0 @@
-import BDetails from './BDetails.vue'
-
-/* istanbul ignore next */
-BDetails.install = function install (Vue) {
-  Vue.component(BDetails.name, BDetails)
-}
-
-export default BDetails
