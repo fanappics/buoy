cd1247498046f9ce23466e49475db42fc50ec9d4
David Sudia <dsudia@gmail.com>
2018-01-19 13:08:55 -0700

    add tests

--- src/components/BContainer/BContainer.test.js
+++ src/components/BContainer/BContainer.test.js
@@ -41,4 +41,84 @@ describe('BContainer', () => {
     expect(wrapper.html()).toMatchSnapshot()
     expect(console.error).toHaveBeenCalledWith('Invalid headerLevel property set. Valid input are numbers 1-6.  Reverting to default level 2.')
   })
+  it('shows an arrow if expandable prop is passed', () => {
+    const label = 'hello world'
+    const headerLevel = 3
+    const wrapper = mount(BContainer, { propsData: { label, headerLevel, expandable: true } })
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+  it('does not show arrow if expandable prop is not passed', () => {
+    const label = 'hello world'
+    const headerLevel = 3
+    const wrapper = mount(BContainer, { propsData: { label, headerLevel } })
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+  it('triggers toggleExpansion when arrow is clicked', () => {
+    const spy = jest.fn()
+    const label = 'hello world'
+    const headerLevel = 3
+    const wrapper = mount(BContainer,
+      {
+        propsData: {
+          label,
+          headerLevel,
+          expandable: true
+        },
+        methods: { toggleExpansion: spy }
+      }
+    )
+    wrapper.find('section > div > span').trigger('click')
+    expect(spy).toHaveBeenCalled()
+  })
+  it('triggers toggleExpansion when focus is on header and enter is pressed', () => {
+    const spy = jest.fn()
+    const label = 'hello world'
+    const headerLevel = 3
+    const wrapper = mount(BContainer,
+      {
+        propsData: {
+          label,
+          headerLevel,
+          expandable: true
+        },
+        methods: { toggleExpansion: spy }
+      }
+    )
+    wrapper.find('section').trigger('keyup.enter')
+    expect(spy).toHaveBeenCalled()
+  })
+  it('triggers toggleExpansion when focus is on header and spacebar is pressed', () => {
+    const spy = jest.fn()
+    const label = 'hello world'
+    const headerLevel = 3
+    const wrapper = mount(BContainer,
+      {
+        propsData: {
+          label,
+          headerLevel,
+          expandable: true
+        },
+        methods: { toggleExpansion: spy }
+      }
+    )
+    wrapper.find('section').trigger('keyup.space')
+    expect(spy).toHaveBeenCalled()
+  })
+  it('should not collapse or expand if expandable prop is not passed', () => {
+    const spy = jest.fn()
+    const label = 'hello world'
+    const headerLevel = 3
+    const wrapper = mount(BContainer,
+      {
+        propsData: {
+          label,
+          headerLevel,
+          expandable: true
+        },
+        methods: { toggleExpansion: spy }
+      }
+    )
+    wrapper.find('section > div > span').trigger('click')
+    expect(wrapper.vm.expanded).toBe(true)
+  })
 })
