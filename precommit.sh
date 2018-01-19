ec959dcbf7d3add0e20bc498f73ff3fe2f72b710
Artem Vovk <avovk@fanatics.com>
2018-01-19 12:07:58 -0700

    finalize

--- precommit.sh
+++ precommit.sh
@@ -1,7 +1,10 @@
-src_change=$(git diff --name-only --cached | grep '.*src/.*' | wc -l)
-doc_change=$(git diff --name-only --cached | grep '.*docs/.*' | wc -l)
-if [ "$src_change" > 0 ] & [ "$doc_change" == 0 ]
+# Check if components were changed
+src_change=`git diff --name-only --cached | grep '.*src/.*' | wc -l`
+# Check if style guide was updated
+doc_change=`git diff --name-only --cached | grep '.*docs/.*' | wc -l`
+
+if [ "$src_change" -gt 0 ] && [ "$doc_change" == 0 ];
 then
-    echo "REBUILD THE STYLEGUIDE, YOU SAVAGE"
+    echo "Remember to rebuild the style guide"
     exit 1
 fi
