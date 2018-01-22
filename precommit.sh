9126f886167ed5c0cc59039e8c459053fde69e84
Artem Vovk <avovk@fanatics.com>
2018-01-22 11:58:03 -0700

    cleaner message

--- precommit.sh
+++ precommit.sh
@@ -5,6 +5,7 @@ doc_change=`git diff --name-only --cached | grep '.*docs/.*' | wc -l`
 
 if [ "$src_change" -gt 0 ] && [ "$doc_change" == 0 ];
 then
-    echo "Remember to rebuild the style guide"
+    echo "Remember to rebuild the style guide!"
+    echo "npm run styleguide:build"
     exit 1
 fi
