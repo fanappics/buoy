1e0428629839ea0c5ab4f17b4661f613a75f0a63
Artem Vovk <avovk@fanatics.com>
2018-02-02 07:50:19 -0700

    prepush print

--- prepush.sh
+++ prepush.sh
@@ -1,8 +1,9 @@
 #! /usr/bin/env bash
 
-echo "Please fill out the PR template."
+
 
 npm run lint
 npm test
 
-echo "The style guide for this branch will be viewable at: https://rawgit.com/fanappics/buoy/$(git branch | awk '/\*/ {print $2}')/docs/index.html"
+printf "\n\nPlease fill out the PR template."
+printf "The style guide for this branch will be viewable at: https://rawgit.com/fanappics/buoy/$(git branch | awk '/\*/ {print $2}')/docs/index.html\n\n"
