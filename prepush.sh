bcd9c090780b64b87284b3370472b59769ccfa9f
Artem Vovk <avovk@fanatics.com>
2018-02-02 07:49:14 -0700

    prepush; remove unused

--- prepush.sh
+++ prepush.sh
@@ -1,6 +1,8 @@
 #! /usr/bin/env bash
 
 echo "Please fill out the PR template."
-echo "The style guide for this branch will be viewable at: https://rawgit.com/fanappics/buoy/$(git branch | awk '/\*/ {print $2}')/docs/index.html"
+
 npm run lint
 npm test
+
+echo "The style guide for this branch will be viewable at: https://rawgit.com/fanappics/buoy/$(git branch | awk '/\*/ {print $2}')/docs/index.html"
