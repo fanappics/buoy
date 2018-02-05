32c3a29806cf2aa1ab49b2e66086d3505b7e5278
Mathew Brown <matt@mattbrown.com>
2018-02-05 11:21:32 -0700

    Ft cpdt 84 (BRadio) (#38)
    * b-radio component with initial tests
    
    * Fixed a few quirky things with accessibility and styling, and added v-model support
    
    * Added export in component index file
    
    * Shortened v-bind and v-on
    
    * Added v-validate required and preliminary css
    
    * Build Styleguide
    
    * Add Radio Documentation
    
    * Styleguide Build
    
    * Build StyleGuide
    
    * Update v-for and key binding.
    
    * Removing vee-validate from dev.  Incorrectly merged from master
    
    * Change documentation text
    
    * Change default radio button from checked property to v-model/value.  This is done for consistancy reasons.
    
    * Update Docs and remove checked property
    
    * Documentation Update.
    
    * Valid is a String.  Previously we assumed Ids were numbers.
    
    * Removed scoped CSS and replace with global CSS rules.
    
    Build Styleguide
    
    * Add check to ensure radio option id does not match group id.
    
    * Update Docs
    
    * Remove border on radio button group
    
    Allow radio button group to be displayed in a row (default) or column format with the boolean prop "column"
    
    * Fix Test
    
    * Build Styelguide

---
+++ src/components/BRadio/BRadio.vue
@@ -0,0 +1,172 @@
+<template>
+  <div class="container" :class="{ disabled: disabled }">
+    <div role="radiogroup" :aria-labelledby="groupId">
+      <label :id="groupId" class="description">
+      {{ groupLabel }}<span v-if="required" aria-hidden="true">*</span>
+      </label>
+      <div class="button-group" :class="{ column: column }" role="radiogroup">
+        <div v-for="(radio, index) in radios" class="button" :key="index">
+          <input type="radio" 
+            v-model="publicValue" 
+            v-validate.initial="index === 0 ? validations : {}"
+            v-bind="index === 0 ? validationAttributes : {}"
+            :aria-checked="publicValue === radio.id"
+            :aria-describedby="errors.any() ? `error-${groupId}` : ''"
+            :aria-labelledby="`${groupId} label-${radio.id}`"
+            :aria-posinset="index"
+            :aria-setsize="radios.length"
+            :disabled="radio.disabled || disabled"
+            :id="radio.id"
+            :name="groupId"
+            :value="radio.id"
+          />
+          <label :id="`label-${radio.id}`" :for="radio.id">
+            {{ radio.value }}
+          </label>
+        </div>
+        <div v-if="errors.any() && showErrors" :id="`error-${groupId}`" class="error-text">
+          <span v-for="(error,index) in errors.all()" :key="index">
+            {{ error }}
+          </span>
+        </div>    
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import validationMixIn from '../../mixins/validation'
+
+export default {
+  name: 'b-radio',
+  mixins: [validationMixIn()],
+  props: {
+    //Required props
+    /**
+    * Each radio group needs a unique id.
+    */
+    groupId: {
+      type: String,
+      required: true
+    },
+    /**
+    * Descriptive text about the radio group.
+    */
+    groupLabel: {
+      type: String,
+      required: true
+    },
+    /**
+    * Array of Objects containing all buttons included in radio group.
+    */
+    radios: {
+      type: Array,
+      required: true,
+      validator (value) {
+        for (let radio of value) {
+          if (!radio.id || !radio.value)
+            throw new TypeError('Each radio must have an id and value defined')
+        }
+        return true
+      }
+    },
+    //Optional props
+    /**
+    * Diplay the radio buttons in a column
+    */
+    column: Boolean,
+    /**
+    * Disables the entire radio group.
+    */
+    disabled: Boolean,
+    /**
+    * Requires a selection to pass validatin. 
+    */
+    required: Boolean,
+    value: String
+  },
+  data () {
+    return {
+      showErrors: false,
+      checked: this.value || ''
+    }
+  },
+  computed: {
+    //Wrapper around value so it is propegated through v-model, or 'public' as I've dubbed it
+    publicValue: {
+      get () {
+        return this.checked
+      },
+      set (value) {
+        this.checked = value.toString()
+        this.$emit('input', this.checked)
+      }
+    },
+    validations () {
+      let vals = {}
+      if (this.required)
+        vals.required = true
+      return vals
+    }
+  },
+  created: function() {
+    const duplicateId = false;
+    
+    for (let i = 0; i < this.radios.length; i++) {
+      if (this.groupId == this.radios[i].id) {
+        throw new Error(`Radio option id ${this.radios[i].id} cannot match GroupId`)
+      } 
+    }
+  },
+  methods: {
+    onValidate (scope) {
+      if (!this.parentScope || this.parentScope === scope) {
+        this.showErrors = true
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+  label {
+    flex-grow: 0;
+  }
+
+  .button {
+    display: flex;
+    align-items: baseline;
+  }
+
+  .button > input {
+    margin-right: .5rem;
+  }
+
+  .button input[type="radio"]:checked+label {
+    font-weight: 600;  
+  }
+
+  input+label {
+    padding-right: .5rem;
+  }
+
+  .button-group {
+    display: flex;
+    flex: 1;
+  }
+
+  .column {
+    flex-direction: column;
+  }
+
+  .container {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+  }
+
+  .description {
+    font-weight: 600;
+  }
+
+</style>
