4b654f40021a68b673895481e89c0bdffb44f817
malear@fanatics.com <malear@fanatics.com>
2018-02-21 12:23:33 -0700

    finishing up css and clicables

--- src/components/BTable/BTable.md
+++ src/components/BTable/BTable.md
@@ -4,13 +4,19 @@ const makeLink = function(data) {
   return `<a href=${ data.id }>${ data.name }</a>`
 }
 
+const makeImage = function(data) {
+  return `<img src="${ data.source }" alt="${ data.name }" width="30" height="30">`
+}
+
 const makeLinkGroup = function(data) {
-  console.log(data)
   let html = ''
   data.forEach(function(action) {
     switch(action) {
       case 'delete':
         html += `<span class="ion-ios-trash" style="color: #4d90fe; font-size: 18px"></span><a href=${ data.id } class='link-group'> Delete</a>`
+        break;
+      case 'edit':
+        html += `<span class="ion-edit" style="color: #4d90fe; font-size: 18px"></span><a href=${ data.id } class='link-group'> Edit</a>`
         break;
     }
   })
@@ -18,9 +24,9 @@ const makeLinkGroup = function(data) {
 }
 
 const headers = [
-  {name: 'Header 1', key: 'header_one'}, 
-  {name: 'Header 2', key: 'header_two', render: makeLink},
-  {name: 'Header 3', key: 'header_three'},
+  {name: 'Header 1', key: 'header_one', filter: true}, 
+  {name: 'Header 2', key: 'header_two', render: makeLink, filter: true},
+  {name: 'Header 3', key: 'header_three', render: makeImage, filter: false},
   {name: 'Header 4', key: 'header_four'},
   {name: 'Header 5', key: 'header_five'},
   {name: 'Header 6', key: 'header_six'},
@@ -29,11 +35,18 @@ const headers = [
 
 
 const tableData = [
-  {header_one: 'column 1', header_two: {name:'column 2', id: 2}, header_three: 'column 3', header_four: 'column 4', header_five: 'column 5', header_six: 'column 6', name: 'example row 1', actions: ['delete']},
-  {header_one: 'column 1', header_two: {name:'column 2', id: 2}, header_three: 'column 3', header_four: 'column 4', header_five: 'column 5', header_six: 'column 6', name: 'example row 2', actions: ['delete']},
-  {header_one: 'column 1', header_two: {name:'column 2', id: 2}, header_three: 'column 3', header_four: 'column 4', header_five: 'column 5', header_six: 'column 6', name: 'example row 3', actions: []}
+  {
+    header_one: 'column 1', 
+    header_two: {name: 'column 2', id: 2}, 
+    header_three: {source:'https://raw.githubusercontent.com/fanappics/buoy/master/buoylogo.png', name: 'buoy logo'}, 
+    header_four: 'column 4', 
+    header_five: 'column 5', 
+    header_six: 'column 6', 
+    name: 'example row 1', 
+    actions: ['delete']
+  },{header_one: 'column 1', header_two: {name: 'column 2', id: 2}, header_three: {source:'https://raw.githubusercontent.com/fanappics/buoy/master/buoylogo.png', name: 'buoy logo'}, header_four: 'column 4', header_five: 'column 5', header_six: 'column 6', name: 'example row 2', actions: ['delete', 'edit']},
+  {header_one: 'column 1', header_two: {name: 'column 2', id: 2}, header_three: {source:'https://raw.githubusercontent.com/fanappics/buoy/master/buoylogo.png', name: 'buoy logo'}, header_four: 'column 4', header_five: 'column 5', header_six: 'column 6', name: 'example row 3', actions: []}
 ]
-
 
 <div>
   <b-table 
--- src/components/BTable/BTable.vue
+++ src/components/BTable/BTable.vue
@@ -1,32 +1,57 @@
 <template>
-  <table class="b-table">
-    <tr class="b-table-row">
-      <th 
-        v-for="header in headers"
-        class="b-table-header"
-      >
-        <button
-        type="button"
+  <div class="b-table">
+    <table class="b-table-el">
+      <tr class="b-table-row">
+        <th 
+          v-for="header in headers"
+          class="b-table-header"
         >
-        {{ header.name }}<img v-if="header.name" class="icon" :src="unsorted" />
-      </button>
-      </th>
-    </tr>
-    <tr
-      v-for="row in rows"
-      class="b-table-row"
-    >
-      <td
-        v-for="cell in row"
-        class="b-table-cell"
-        v-html='cell'
-      />
-      </td>
-    </tr>
-  </table>
+          <button
+          type="button"
+          class="header-button"
+          @click="sortColumn(header.key)"
+          :disabled="!header.filter"
+          >
+            {{ header.name }}<img v-if="header.name && header.filter" class="icon" :src="unsorted" />
+          </button>
+        </th>
+      </tr>
+      <tr
+        v-for="row in rows"
+        class="b-table-row"
+      >
+        <td
+          v-for="cell in row"
+          class="b-table-cell"
+          v-html='cell'
+        />
+        </td>
+      </tr>
+    </table>
+    <div class="pagination">
+      <div class="pagination-buttons">
+        <button
+          type="button" 
+          class="pagination-button"
+          @click="paginate(currentPage-1)"
+          :disabled="currentPage <= 0"
+        >
+          <i class='pagination-icon ion-arrow-left-c' aria-hidden='true'></i>
+        </button>
+        <button
+          type="button"
+          class="pagination-button"
+          @click="paginate(currentPage+1)"
+        >
+          <i class='pagination-icon ion-arrow-right-c' aria-hidden='true'></i>
+        </button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+
 export default {
 
   name: 'b-table',
@@ -46,12 +71,24 @@ export default {
     tableData: {
       type: Array,
       required: true
+    },
+    pageNumber: {
+      type: Number,
+      required: false
+    },
+    rowsPerPage: {
+      type: Number,
+      default: 10
+    },
+    totalResults: {
+      type: Number,
     }
   },
   data () {
     return {
       rows: this.buildRows(this.tableData, this.headers),
-      unsorted: require('../../../static/unSortedArrows.svg')
+      unsorted: require('../../../static/unSortedArrows.svg'),
+      currentPage: this.pageNumber ? this.pageNumber : 0
     }
   },
   methods: {
@@ -69,6 +106,14 @@ export default {
         rows.push(row)
       })
       return rows
+    },
+    sortColumn: function(sortBy) {
+      this.$emit("sort", sortBy)
+    },
+    paginate: function(page) {
+      this.$emit("paginate", page)
+      this.currentPage = page
+      console.log(this.currentPage)
     }
   }
 
@@ -77,7 +122,7 @@ export default {
 
 <style scoped>
 
-  .b-table {
+  .b-table-el {
     width: 100%;
     border-collapse: collapse;
   }
@@ -106,9 +151,42 @@ export default {
     font-weight: 600;
   }
 
+  button:focus {
+    outline: 0;
+    border: none;
+  }
+
+  button.header-button:disabled {
+    color: #000000;
+    opacity: 100;
+    cursor: default;
+  }
+
   img.icon {
     position: absolute;
     margin-left: .725rem;
+  }
+
+  .pagination {
+    border-style: solid none solid none;
+    border-width: thin;
+    width: 100%;
+    text-align: right;
+    height: 2.5rem;
+    border-color: #c4c3c4;
+  }
+
+  .pagination-icon {
+    color: #4D90FE;
+    font-size: 22px;
+  }
+
+  .pagination-buttons{
+    padding-right: .5rem;
+  }
+  .pagination-button{
+    padding: 0 .5rem;
+    margin-top: .5rem
   }
 
 </style>
