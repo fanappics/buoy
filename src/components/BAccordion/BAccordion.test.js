f877ad759f580de6f691c00e136be09754f9ab27
emallory <emallory@fanatics.com>
2018-02-28 10:14:57 -0700

    Make Accordions Great (Again?)

--- src/components/BAccordion/BAccordion.test.js
+++ src/components/BAccordion/BAccordion.test.js
@@ -1,47 +1,58 @@
-import { mount } from 'vue-test-utils'
+import { mount, shallow } from 'vue-test-utils'
 import BAccordion from './BAccordion'
 
 describe('BAccordion', () => {
-  it('triggers toggleExpansion when header button is clicked', () => {
-    const spy = jest.fn()
-    const wrapper = mount(BAccordion,
-      {
-        propsData: {
-          id: 'accordion-1',
-          label: 'hello world'
-        },
-        methods: { toggleExpansion: spy }
+  it('mounted html should match snapshot', () => {
+    const wrapper = shallow(BAccordion, {
+      propsData: {
+        id: 'accordion-1',
+        label: 'hello world'
       }
-    )
-    wrapper.find('section > div > button').trigger('click')
-    expect(spy).toHaveBeenCalled()
+    })
+    expect(wrapper.html()).toMatchSnapshot()
   })
-  it('triggers toggleExpansion when focus is on header and enter is pressed', () => {
-    const spy = jest.fn()
-    const wrapper = mount(BAccordion,
-      {
-        propsData: {
-          id: 'accordion-1',
-          label: 'hello world'
-        },
-        methods: { toggleExpansion: spy }
-      }
-    )
-    wrapper.find('section > div > button').trigger('keyup.enter')
-    expect(spy).toHaveBeenCalled()
-  })
-  it('triggers toggleExpansion when focus is on header and spacebar is pressed', () => {
-    const spy = jest.fn()
-    const wrapper = mount(BAccordion,
-      {
-        propsData: {
-          id: 'accordion-1',
-          label: 'hello world'
-        },
-        methods: { toggleExpansion: spy }
-      }
-    )
-    wrapper.find('section > div > button').trigger('keyup.space')
-    expect(spy).toHaveBeenCalled()
+  describe('header actions', () => {
+    it('triggers toggleExpansion when header button is clicked', () => {
+      const spy = jest.fn()
+      const wrapper = mount(BAccordion,
+        {
+          propsData: {
+            id: 'accordion-1',
+            label: 'hello world'
+          },
+          methods: { toggleExpansion: spy }
+        }
+      )
+      wrapper.find('section > div > button').trigger('click')
+      expect(spy).toHaveBeenCalled()
+    })
+    it('triggers toggleExpansion when focus is on header and enter is pressed', () => {
+      const spy = jest.fn()
+      const wrapper = mount(BAccordion,
+        {
+          propsData: {
+            id: 'accordion-1',
+            label: 'hello world'
+          },
+          methods: { toggleExpansion: spy }
+        }
+      )
+      wrapper.find('section > div > button').trigger('keyup.enter')
+      expect(spy).toHaveBeenCalled()
+    })
+    it('triggers toggleExpansion when focus is on header and spacebar is pressed', () => {
+      const spy = jest.fn()
+      const wrapper = mount(BAccordion,
+        {
+          propsData: {
+            id: 'accordion-1',
+            label: 'hello world'
+          },
+          methods: { toggleExpansion: spy }
+        }
+      )
+      wrapper.find('section > div > button').trigger('keyup.space')
+      expect(spy).toHaveBeenCalled()
+    })
   })
 })
